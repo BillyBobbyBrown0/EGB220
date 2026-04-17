063d3496c50ced9c1fb3e9eef5c634cb6fa2c4a6
BillyBobbyBrown0 <n11609273@qut.edu.au>
2026-04-17 11:07:49 +1000

    Update Servo.ino

--- Servo.ino
+++ Servo.ino
@@ -1,18 +1,20 @@
-#include "arduino_secrets.h"
-
 #include<Arduino.h>
 #include <Servo.h>
 
 Servo S1;
+Servo S2;
 
 //THIS WILL ALL BE ON THE TINSEY as the servo is mounted on the tinsey
 void setup() {
-  pinMode(9, OUTPUT);
-  S1.attach(9);
+  S1.attach(9);   // First servo on pin 9
+  S2.attach(10);  // Second servo on pin 10
 
 }
 
 void loop() {
-  S1.write(255); //This will go to the maximum
-
+  for (int angle = 0; angle <= 180; angle++) {
+    S1.write(angle);
+    S2.write(180-angle); // opposite direction
+    delay(20);
+  }
 }
